6f5ec6651599ce2d22c4c8ea41fe23462a287857
Gurava <gurava.samtimi@gmail.com>
2017-09-12 11:24:39 +0530

    Initial commit

---
+++ COOLStagingMirror/src/main/java/COOL/COOLStagingMirror/App.java
@@ -0,0 +1,13 @@
+package COOL.COOLStagingMirror;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ COOLStagingMirror/src/test/java/com/coolstagingmirror/tests/COOLStaggingMirrorTest.java
@@ -0,0 +1,5 @@
+package com.coolstagingmirror.tests;
+
+public class COOLStaggingMirrorTest {
+//https://bldbz173082.cloud.dst.ibm.com/login
+}
